5ec1733e20688cc72d10b281e990bdf7b34b5394
Alexander Mustafin <sashker@users.noreply.github.com>
2022-07-18 15:39:59 +0300

    Calculation and breakdown table

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "df696bd866cde9e9015d41f160014c22",
-"index.html": "ad89527a1bbadbe14aa6a0d2c6658d3b",
-"/": "ad89527a1bbadbe14aa6a0d2c6658d3b",
-"main.dart.js": "c7d07d3844214611ff9984afc885aa68",
-"flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
+"index.html": "305f879ff266714d9cbc10a8f856b3f9",
+"/": "305f879ff266714d9cbc10a8f856b3f9",
+"main.dart.js": "984fb18a227899ae268b831a958ab9b2",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -20,11 +19,7 @@ const RESOURCES = {
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
 "assets/assets/fonts/Helvetica_Neue_Medium_It.ttf": "4361c3ce34740903e3af9d48d8a941ff",
-"assets/assets/fonts/Helvetica_Neue_Medium_Extended.ttf": "365834cfa7beb7ca64c00476397ddc32",
-"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
-"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
-"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
-"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba"
+"assets/assets/fonts/Helvetica_Neue_Medium_Extended.ttf": "365834cfa7beb7ca64c00476397ddc32"
 };
 
 // The application shell files that are downloaded before a service worker can
